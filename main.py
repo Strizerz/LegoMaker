a39e343336e2921b2af8fc94fd8e76597017bbce
Aadesh Bhattarai <Aadeshwins55@gmail.com>
2025-07-24 18:33:05 -0600

    voxels created

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import trimesh
 
-path = "src\Car.obj"
-
+path = "src\\Car.obj"
+#load the model
 mesh = trimesh.load(path)
 
-mesh.show()
+#change the model from a scene to the first object which is usually the model 
+mesh = trimesh.util.concatenate(tuple(mesh.geometry.values()))
+
+#quality or size of the voxels, lower value is higher quality
+quality = 0.2
+
+#turn it into a grid of voxels without a mesh
+voxelGrid = mesh.voxelized(pitch=quality)
+
+#turn the voxel grid back to a mesh to visualize
+voxelMesh = voxelGrid.as_boxes()
+
+#show the model
+voxelMesh.show()
